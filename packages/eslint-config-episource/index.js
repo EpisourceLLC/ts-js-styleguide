96d6df6e2dbc261c60f3d44b5ccfae8b4ea4ab7f
Ivan Santos <pragmaticivan@gmail.com>
2019-08-14 14:39:05 -0700

    feat: add configs for eslint config for react

---
+++ packages/eslint-config-episource/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'eslint-config-episource-base',
+    './rules/react',
+    './rules/react-a11y',
+  ].map(require.resolve),
+  parser: require.resolve('babel-eslint'),
+  rules: {},
+};
---
+++ packages/eslint-config-episource/jest.js
@@ -0,0 +1,2 @@
+
+module.exports = require('eslint-config-episource-base/jest');
