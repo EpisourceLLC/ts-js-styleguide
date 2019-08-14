8d2c0971ff61763f8931141830be9e94eb8865f4
Ivan Santos <pragmaticivan@gmail.com>
2019-08-14 16:44:01 -0700

    feat: add prettier config support to eslint main pkg

--- packages/eslint-config-episource/index.js
+++ packages/eslint-config-episource/index.js
@@ -1,7 +1,10 @@
 module.exports = {
-  extends: ['eslint-config-episource-base', './rules/react', './rules/react-a11y'].map(
-    require.resolve
-  ),
+  extends: [
+    'eslint-config-episource-base',
+    './rules/react',
+    './rules/react-a11y',
+    'eslint-config-prettier/react',
+  ].map(require.resolve),
   parser: require.resolve('babel-eslint'),
   rules: {},
 };
