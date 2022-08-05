34b5e4558d3b7c1864136bef6435327d65e19310
Isaac Hernandez <isaac.hernandez@LAX-LM11746.local>
2022-08-05 12:39:26 -0700

    feat(sc-48732): make security-node plugin cause errors, include docs

--- packages/eslint-config-episource-base/rules/security-node.js
+++ packages/eslint-config-episource-base/rules/security-node.js
@@ -1,26 +1,71 @@
 module.exports = {
+  // plugin npm page: https://www.npmjs.com/package/eslint-plugin-security-node
+  // plugin rule docs on git: https://github.com/gkouziik/eslint-plugin-security-node/tree/master/docs/rules
   rules: {
-    'security-node/detect-absence-of-name-option-in-exrpress-session': 'warn',
-    'security-node/detect-buffer-unsafe-allocation': 'warn',
-    'security-node/detect-child-process': 'warn',
-    'security-node/detect-crlf': 'warn',
-    'security-node/detect-dangerous-redirects': 'warn',
-    'security-node/detect-eval-with-expr': 'warn',
-    'security-node/detect-html-injection': 'warn',
-    'security-node/detect-improper-exception-handling': 'warn',
-    'security-node/detect-insecure-randomness': 'warn',
-    'security-node/detect-non-literal-require-calls': 'warn',
-    'security-node/detect-nosql-injection': 'warn',
-    'security-node/detect-option-multiplestatements-in-mysql': 'warn',
-    'security-node/detect-option-rejectunauthorized-in-nodejs-httpsrequest': 'warn',
-    'security-node/detect-option-unsafe-in-serialize-javascript-npm-package': 'warn',
-    'security-node/detect-possible-timing-attacks': 'warn',
-    'security-node/detect-runinthiscontext-method-in-nodes-vm': 'warn',
-    'security-node/detect-security-missconfiguration-cookie': 'warn',
-    'security-node/detect-sql-injection': 'warn',
-    'security-node/detect-unhandled-async-errors': 'warn',
-    'security-node/detect-unhandled-event-errors': 'warn',
-    'security-node/disable-ssl-across-node-server': 'warn',
-    'security-node/non-literal-reg-expr': 'warn',
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-absence-of-name-option-in-exrpress-session.md
+    'security-node/detect-absence-of-name-option-in-exrpress-session': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-buffer-unsafe-allocation.md
+    'security-node/detect-buffer-unsafe-allocation': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-child-process.md
+    'security-node/detect-child-process': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-crlf.md
+    'security-node/detect-crlf': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-dangerous-redirects.md
+    'security-node/detect-dangerous-redirects': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-eval-with-expr.md
+    'security-node/detect-eval-with-expr': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-html-injection.md
+    'security-node/detect-html-injection': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-improper-exception-handling.md
+    'security-node/detect-improper-exception-handling': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-insecure-randomness.md
+    'security-node/detect-insecure-randomness': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-non-literal-require-calls.md
+    'security-node/detect-non-literal-require-calls': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-nosql-injection.md
+    'security-node/detect-nosql-injection': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-option-multiplestatements-in-mysql.md
+    'security-node/detect-option-multiplestatements-in-mysql': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-option-rejectunauthorized-in-nodejs-httpsrequest.md
+    'security-node/detect-option-rejectunauthorized-in-nodejs-httpsrequest': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-option-unsafe-in-serialize-javascript-npm-package.md
+    'security-node/detect-option-unsafe-in-serialize-javascript-npm-package': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-possible-timing-attacks.md
+    'security-node/detect-possible-timing-attacks': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-runinthiscontext-method-in-nodes-vm.md
+    'security-node/detect-runinthiscontext-method-in-nodes-vm': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-security-missconfiguration-cookie.md
+    'security-node/detect-security-missconfiguration-cookie': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-sql-injection.md
+    'security-node/detect-sql-injection': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-unhandled-async-errors.md
+    'security-node/detect-unhandled-async-errors': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/detect-unhandled-event-errors.md
+    'security-node/detect-unhandled-event-errors': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/disable-ssl-across-node-server.md
+    'security-node/disable-ssl-across-node-server': 'error',
+
+    // https://github.com/gkouziik/eslint-plugin-security-node/blob/master/docs/rules/non-literal-reg-expr.md
+    'security-node/non-literal-reg-expr': 'error',
   },
 };
