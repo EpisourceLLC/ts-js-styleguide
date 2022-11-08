dd2304b3e21c6a675c277bf2bf4eb543e4c50daf
Elliott Foster <elliott.foster@episource.com>
2022-11-08 14:13:40 -0600

    feat: add pr rules
    These rules can be used to test commits to align with desired PR formatting.
    
    @episource/dependencies - tests to ensure commit messages include a
    dependencies array, e.g. 'feat: thing [medicare-api]'
    
    @episource/story-link - tests to ensure commit messages include a
    link to the story in the scope field, e.g. 'feat(sc-123): thing'

--- packages/commitlint-config/commitlint.config.js
+++ packages/commitlint-config/commitlint.config.js
@@ -1,6 +1,40 @@
+const STORY_REGEX = /^sc-\d+/;
+const DEPS_REGEX = /\[[^\]]*\]$/;
+
+const withWhen =
+  (when = 'always') =>
+  test => {
+    if (when === 'always') {
+      return !!test;
+    }
+    return !test;
+  };
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-case': [2, 'always', ['kebab-case']],
   },
+  plugins: [
+    {
+      rules: {
+        '@episource/dependencies': function dependencies(parsed, when) {
+          const test = withWhen(when);
+          const { subject } = parsed;
+          return [
+            test(subject && !!subject.match(DEPS_REGEX)),
+            'Your subject must contain a dependencies array, e.g. "feat: did the thing [dependency1]".\n\tUse an empty array if there are no dependencies.',
+          ];
+        },
+        '@episource/story-link': function storyLink(parsed, when) {
+          const test = withWhen(when);
+          const { scope } = parsed;
+          return [
+            test(scope && !!scope.match(STORY_REGEX)),
+            'Your scope must contain a shortcut story link, e.g. "feat(sc-123): did the thing"',
+          ];
+        },
+      },
+    },
+  ],
 };
